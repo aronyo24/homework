f220e35b0f06e4d5b9fbd60a9f91b39735a30b98
Aronyo Mojumder <114594204+aronyo24@users.noreply.github.com>
2025-10-19 01:57:09 +0600

    update app

--- section4/views.py
+++ section4/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Robust view for section3. Keeps import-time simple to avoid AttributeError
 def section4_view(request):
-    return render(request, 'section3.html')
+    return render(request, 'section4.html')
 
--- section5/views.py
+++ section5/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Robust view for section4. Keeps import-time simple to avoid AttributeError
 def section5_view(request):
-    return render(request, 'section3.html')
+    return render(request, 'section5.html')
 
